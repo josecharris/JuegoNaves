fd229fad1a3b5f184d6f0c141901295da105126b
Jose Charris <jose.charris@uptc.edu.co>
2019-08-27 11:34:11 -0500

    Pintar limites, nave y detalles

--- nave.cpp
+++ nave.cpp
@@ -28,25 +28,105 @@ void hide_coursor(){
 	SetConsoleCursorInfo(hCon, &info);
 }
 
+void pintar_limites(){
+	for(int i=2;i<78; i++){
+		gotoxy(i,3);
+		printf("%c",205);
+		gotoxy(i,33);
+		printf("%c",205);
+	}
+	
+	for(int i=4;i<33;i++){
+		gotoxy(2,i);
+		printf("%c",186);
+		gotoxy(77,i);
+		printf("%c",186);
+	}
+	
+	gotoxy(2,3); printf("%c",201);
+	gotoxy(2,33); printf("%c",200);
+	gotoxy(77,3); printf("%c",187);
+	gotoxy(77,33); printf("%c",188);
+}
+
+class Nave{
+	int x, y;
+	int corazones;
+	public:
+		Nave(int _x, int _y, int _corazones);
+		void pintar();
+		void borrar();
+		void mover();
+		void pintar_corazones();
+};
+
+Nave::Nave(int _x, int _y, int _corazones){
+	x=_x;
+	y=_y;
+	corazones = _corazones;
+}
+
+void Nave::pintar(){
+	gotoxy(x, y);
+	printf("  %c",30);
+	gotoxy(x, y+1);
+	printf(" %c%c%c",40,207,41);
+	gotoxy(x, y+2);
+	printf("%c%c %c%c",30,190,190,30);
+	
+}
+
+void Nave::borrar(){
+	gotoxy(x, y);
+	printf("     ");
+	gotoxy(x, y+1);
+	printf("     ");
+	gotoxy(x, y+2);
+	printf("     ");
+}
+
+void Nave::mover(){
+	if(kbhit()){
+		char tecla = getch();
+		borrar();
+		if(tecla == IZQUIERDA && x > 3){	x--;	}
+		if(tecla == DERECHA && x+6 < 77){	x++;	}
+		if(tecla == ARRIBA && y>4){	y--;	}
+		if(tecla == ABAJO && y+3 < 33){		y++;	}
+		pintar();
+		pintar_corazones();
+	}
+}
+
+void Nave::pintar_corazones(){
+	gotoxy(64,2);
+	printf("Salud");
+	gotoxy(70,2);
+	printf("     ");
+	for(int i=0; i<corazones;i++){
+		gotoxy(70+i,2);
+		printf("%c",3);
+	}
+}
+
+
 int main(){
 	hide_coursor();
-	int x = 10,	y = 10;
-	gotoxy(x,y);
-	cout<<"*"<<endl;
+	pintar_limites();
+	Nave nave = Nave(7,7,3);
+	nave.pintar();
 	bool game_over = false;
 	while(!game_over){
-		if(kbhit()){
-			char tecla = getch();
-			gotoxy(x,y);
-			cout<<" "<<endl;
-			if(tecla == IZQUIERDA){	x--;	}
-			if(tecla == DERECHA){	x++;	}
-			if(tecla == ARRIBA){	y--;	}
-			if(tecla == ABAJO){		y++;	}
-			gotoxy(x,y);
-			cout<<"*"<<endl;
-		}
+		nave.mover();
 		Sleep(30);
 	}
 	return 0;
 }
+
+
+
+
+
+
+
+
