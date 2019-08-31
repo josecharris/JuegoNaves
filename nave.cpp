1296b0613a58cea377dc40f6be31c4356ef18f51
Jose Charris <jose.charris@uptc.edu.co>
2019-08-31 18:26:24 -0500

    Asteroides

--- nave.cpp
+++ nave.cpp
@@ -2,11 +2,13 @@
 #include<stdio.h>
 #include<windows.h>
 #include<conio.h>
+#include<stdlib.h>
 
 #define ARRIBA 72
 #define ABAJO 80
 #define DERECHA 77
 #define IZQUIERDA 75
+#define LETRA_A 97
 
 using namespace std;
 
@@ -52,18 +54,21 @@ void pintar_limites(){
 class Nave{
 	int x, y;
 	int corazones;
+	int vida;
 	public:
-		Nave(int _x, int _y, int _corazones);
+		Nave(int _x, int _y, int _corazones, int _vidas);
 		void pintar();
 		void borrar();
 		void mover();
 		void pintar_corazones();
+		void morir();
 };
 
-Nave::Nave(int _x, int _y, int _corazones){
+Nave::Nave(int _x, int _y, int _corazones, int _vidas){
 	x=_x;
 	y=_y;
 	corazones = _corazones;
+	vida = _vidas;
 }
 
 void Nave::pintar(){
@@ -78,11 +83,11 @@ void Nave::pintar(){
 
 void Nave::borrar(){
 	gotoxy(x, y);
-	printf("     ");
+	printf("        ");
 	gotoxy(x, y+1);
-	printf("     ");
+	printf("        ");
 	gotoxy(x, y+2);
-	printf("     ");
+	printf("        ");
 }
 
 void Nave::mover(){
@@ -93,12 +98,15 @@ void Nave::mover(){
 		if(tecla == DERECHA && x+6 < 77){	x++;	}
 		if(tecla == ARRIBA && y>4){	y--;	}
 		if(tecla == ABAJO && y+3 < 33){		y++;	}
+		if(tecla == LETRA_A){		corazones--;	}
 		pintar();
 		pintar_corazones();
 	}
 }
 
 void Nave::pintar_corazones(){
+	gotoxy(50,2);
+	printf("Vidas %d", vida);
 	gotoxy(64,2);
 	printf("Salud");
 	gotoxy(70,2);
@@ -109,14 +117,65 @@ void Nave::pintar_corazones(){
 	}
 }
 
+void Nave::morir(){
+	if(corazones==0){
+		borrar(); //BORRA NAVE
+		gotoxy(x,y);   printf("   **      ");
+		gotoxy(x,y+1); printf("  ****     ");
+		gotoxy(x,y+2); printf("   **      ");
+		Sleep(200);
+		borrar();
+		gotoxy(x,y);   printf(" * ** *");
+		gotoxy(x,y+1); printf("  **** ");
+		gotoxy(x,y+2); printf(" * ** *");
+		Sleep(200);
+		borrar();
+		vida--;
+		corazones=3;
+		pintar_corazones();
+		pintar();
+	}
+}
+
+class Asteroide{
+	int x,y;
+	public:
+		Asteroide(int _x,int _y);
+		void pintar();
+		void mover();
+		
+};
+
+Asteroide::Asteroide(int _x, int _y){
+	x = _x;
+	y = _y;
+}
+
+void Asteroide::pintar(){
+	gotoxy(x,y);
+	printf("%c",184);
+}
+
+void Asteroide::mover(){
+	gotoxy(x,y); printf(" ");
+	y++;
+	if(y>32){
+		x = rand()%71 + 4;
+		y = 4;
+	}
+	pintar();
+}
 
 int main(){
 	hide_coursor();
 	pintar_limites();
-	Nave nave = Nave(7,7,3);
+	Nave nave = Nave(7,7,3,3);
 	nave.pintar();
 	bool game_over = false;
+	Asteroide asteroide(10, 4);
 	while(!game_over){
+		asteroide.mover();
+		nave.morir();
 		nave.mover();
 		Sleep(30);
 	}
